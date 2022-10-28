ce0baf2e4a50c9e6d5675cc98bd18ddeb1385aca
Thanh Cong <ducong45@gmail.com>
2022-10-29 02:03:14 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const cors = require('cors');
+const contactController = require('./controllers/contact.controller');
+const ApiError = require('./api-error');
 
 const app = express();
-const contactController = require('./controllers/contact.controller');
 
 app.use(cors());
 app.use(express.json());
@@ -12,10 +13,31 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
 });
 
-// app.route('/api/contacts')
-//     .get(contactController.findAll)
-//     .post(contactController.create)
-//     .delete(contactController.deleteAll);
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+// Handle 404 response.
+app.use((req, res, next) => {
+    // Handler for unknown route.
+    // Call next() to pass to the error handling middleware.
+    return next(new ApiError(404, 'Resource not found'));
+});
+// Define error-handling middleware last, after other app.use() and routes calls.
+    app.use((error, req, res, next) => {
+        // The centralized error handling middleware.
+        // In any route handler, calling next(error)
+        // will pass to this error handling middleware.
+        return res.status(error.statusCode || 500).json({
+            message: error.message || 'Internal Server Error',
+        });
+    });
+
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
 
 // app.route('/api/contacts/favorite')
 //     .get(contactController.findAllFavorite);
