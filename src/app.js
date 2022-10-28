e45abc0aa441eabd0d176a171428545b23cb830a
Thanh Cong <ducong45@gmail.com>
2022-10-29 04:05:54 +0700

    Implement handlers

--- src/app.js
+++ src/app.js
@@ -13,37 +13,32 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
 });
 
-app.route('/api/contacts/:id')
-    .get(contactController.findOne)
-    .put(contactController.update)
-    .delete(contactController.delete);
-
-// Handle 404 response.
-app.use((req, res, next) => {
-    // Handler for unknown route.
-    // Call next() to pass to the error handling middleware.
-    return next(new ApiError(404, 'Resource not found'));
-});
-// Define error-handling middleware last, after other app.use() and routes calls.
-    app.use((error, req, res, next) => {
-        // The centralized error handling middleware.
-        // In any route handler, calling next(error)
-        // will pass to this error handling middleware.
-        return res.status(error.statusCode || 500).json({
-            message: error.message || 'Internal Server Error',
-        });
-    });
-
 app.route('/api/contacts')
     .get(contactController.findAll)
     .post(contactController.create)
     .delete(contactController.deleteAll);
 
+    
 // app.route('/api/contacts/favorite')
 //     .get(contactController.findAllFavorite);
-
+    
 app.route('/api/contacts/:id(\\d+)')
     .get(contactController.findOne)
     .put(contactController.update)
     .delete(contactController.delete);
+    
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
+});
+
 module.exports = app;
