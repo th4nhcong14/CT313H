252c62f17fdfc919c9f21bcd0f0a260830bdc04a
Thanh Cong <ducong45@gmail.com>
2022-10-29 00:21:37 +0700

    Install express and show a welcome message

---
+++ src/app.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to contact book application.' });
+});
+
+module.exports = app;
